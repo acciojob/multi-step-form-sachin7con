1246e4b3d2afce68b72a2a5be816218df3ad716c
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-08-10 17:55:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,50 @@ import React, { useState } from 'react';
 import './styles/App.css'; // Ensure the correct path
 import Step from './Step';
 
-
 const App = () => {
-    const[currentStep, setCurrentStep] = useState(1);
+  const [currentStep, setCurrentStep] = useState(1);
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    model: '',
+    car_price: '',
+    card_info: '',
+    expiry_date: ''
+  });
 
-    const handleNext = () => {
-      setCurrentStep((prevStep) => prevStep + 1);
-    }
-    const handlePrev = () => {
+  const handleNext = () => {
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  const handlePrev = () => {
     setCurrentStep((prevStep) => prevStep - 1);
-  }
+  };
+
   const handleSubmit = () => {
-      console.log("Submitted");
-  }
+    console.log('Submitted:', formData);
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value
+    });
+  };
 
   return (
     <div>
-        {/*SGN Do not remove the main div */}
-        <Step currentStep={currentStep} onNext={handleNext} onPrev={handlePrev} onSubmit={handleSubmit} />
-
+      {/* Do not remove the main div */}
+      <Step
+        currentStep={currentStep}
+        formData={formData}
+        handleChange={handleChange}
+        onNext={handleNext}
+        onPrev={handlePrev}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 };
 
-export default App
+export default App;
